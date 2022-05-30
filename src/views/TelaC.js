5ff7c4d0b482c5cf7865cb0691da6ed28906f22d
Rogério Santos <rogeriosantosdev@gmail.com>
2022-05-30 18:49:06 -0300

    navegação stack

---
+++ src/views/TelaC.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TextoCentral from '../components/TextoCentral'
+
+export default props => {
+    const route =  props.route || {params: {numero: 0}}
+    return (
+        <TextoCentral corFundo='#9932cd'>
+            Tela C - {route.params.numero}
+        </TextoCentral>
+    )
+}
